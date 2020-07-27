cda563c705384b6f6efa639f2768b0b2d52534e5
Janahan Ravi <janahanravi10@gmail.com>
2020-07-27 00:51:40 -0400

    Added gitignore and created methods for reading command line

---
+++ Lox.java
@@ -0,0 +1,56 @@
+package com.craftinginterpreters.lox;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class Lox {
+    public static void main(String[] args) throws IOException {
+        if (args.length > 1) {
+            System.out.println("Usage: jlox[script]");
+            System.exit(64)
+        } else if (args.length == 1) {
+            // Takes command line argument and runs it
+            runFile(args[0]);
+        } else {
+            runPrompt();
+        }
+    }
+
+    // Reading file from command line using path to file given
+    private static void runFile(String path) throws IOException {
+        byte[] bytes = Files.readAllBytes(Paths.get(path));
+        run(new String(bytes, Charset.defaultCharset()));
+    }
+
+    // Getting prompts from command line
+    private static void runPrompt() throws IOException {
+        InputStreamReader input = new InputStreamReader(System.in);
+        BufferedReader reader = new BufferedReader(input);
+
+        for (;;) {
+            System.out.print("> ");
+            String line = reader.readLine();
+
+            // When crtl+D is entered the loop will break
+            if (line == null) {
+                break;
+            }
+            run(line);
+        }
+    }
+    // Running actual file
+    private static run(String source) {
+        Scanner input = new Scanner(source);
+        // Scanning expressions for parsing
+        List<Token> tokens = input.scanTokens();
+        // Just printing for now **TAKE OUT LATER**
+        for (Token token : tokens) {
+            System.out.println(token);
+        }
+    }
+}
